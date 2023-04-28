2ad72cb102752df5ced591b21b4eb6e8ecfeaffa
Vinod Gullipalli <vinod.g@doodleblue.com>
2023-04-28 10:05:06 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const arr = [
+    {
+      id: 1,
+      title: "React Js",
+    },
+    {
+      id: 2,
+      title: "Node js",
+    },
+    {
+      id: 3,
+      title: "Bootstrap",
+    },
+    { id: 4, title: "Material Ui" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {arr.map((item) => {
+        return <p key={item}>{item.title}</p>;
+      })}
+      <h1>Count:{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Dec</button>
     </div>
   );
+  // const Fruits = ["orange", "apple", "guvava"];
+  // const [name, addName] = useState("");
+  // const [data, setData] = useState(["vinod"]);
+  // const [value, setValue] = useState("");
+  // const getData = () => {
+  //   setData(Fruits);
+  // };
+  // const addData = () => {
+  //   setData([...data, name]);
+  // };
+  // return (
+  //   <div className="App">
+  //     {/* //   <input list="data" onClick={getData} /> */}
+  //     <input type="text" onChange={(e) => addName(e.target.value)} />
+  //     <button onClick={addData}>Add</button>
+  //     {data.map((item) => (
+  //       <p>{item}</p>
+  //     ))}
+  // {
+  //   /* <datalist id="data">
+  //       (
+  //       {data.map((item) => (
+  //         <option>{item}</option>
+  //       ))}
+  //     //   )
+  //     // </datalist> */
+  // }
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
